d3268b174fe6e8647b660fa72509f2e22262fc4b
126BDuchovny <126bduchovny@frhsd.com>
2025-11-21 12:38:11 -0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useNavigate} from "react-router";
+import {Outlet} from "react-router";
+import {MobileDateTimePicker} from "@mui/x-date-pickers";
 import './App.css'
+// import {useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
+
+
+export function Navbar(){
+    const navigate = useNavigate();
+    const gotoProduct = () => {
+        navigate('/products')
+    }
+    const gotoHome = () =>
+    {
+        navigate('/');
+    }
+    const gotoCart = () =>{
+        navigate('/cart')
+    }
+    return (<>
+        <div className={"NavBar"}>
+            <button id = "goHome" onClick = {gotoHome}>Home</button>
+            <button id = "goToCart" onClick = {gotoCart}>Cart</button>
+            <button id = "goToProduct" onClick = {gotoProduct}>Confirmation</button>
+        </div>
+        <div id={"mainScreen"}>
+            <Outlet/>
+        </div>
+        </>)
 }
 
-export default App
+export function Services(){
+    return (
+        <div>
+            <h1 id="products"> Services</h1>
+            <p id="whatWeDo"> We help people with dirty cars and maje them clean again</p>
+            <h3 id = "targetTitle"> Target Audience</h3>
+            <div className = "targetAudience">
+                <div className = {"everyone"}>
+                    <h5>Everyone</h5>
+                    <p>We want to help all people who have dirty cars. We want everyone to have a clean car to use so we are happy to help anyone that needs it</p>
+                </div>
+            </div>
+            <div className = "calander">
+                <h5>Book when you want your car to be cleaned here</h5>
+                <MobileDateTimePicker onAccept={(value)=>{let dates = [];
+                    if(localStorage.getItem("timesBooked") != null){
+                        dates = JSON.parse(localStorage.getItem("timesBooked") as string);
+                    }
+                    dates.push(value);
+                    localStorage.setItem("timesBooked", JSON.stringify(dates));
+                }} />
+            </div>
+        </div>
+    )
+}
+
+export function Home(){
+            return (
+                <div>
+                    <h1 id="home">Home</h1>
+                    <p id="Welcome">Welcome to Making Cars greener</p>
+                </div>
+            )
+}
+
+export function Confirmation(){
+            // function getItems(key:string):string[]{
+            //     const item = localStorage.getItem(key);
+            //     return item ? JSON.parse(item) as string[] : [];
+            // }
+    //         const [items] = useState(getItems("daysBooked"));
+    //
+    //         fuction dayOfWeek(dayOfWeek: number){
+    //             if(dayOfWeek == 0){
+    //                 return "Sunday"
+    //             }
+    // }
+    // fuction month(monthNumber: number){
+    //
+    // }
+     return(
+         <div className = {"confirmation"}>
+
+         </div>
+
+    )
+}
+export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {HashRouter, Route, Routes} from "react-router";
+import Home, { Confirmation, Navbar, Services} from "./App.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Navbar />}>
+                    <Route index element={<Home/>} />
+                    <Route path="services" element={<Services/>}/>
+                    <Route path="confirmation" element={<Confirmation/>} />
+                </Route>
+            </Routes>
+        </HashRouter>
+    </StrictMode>,
 )
